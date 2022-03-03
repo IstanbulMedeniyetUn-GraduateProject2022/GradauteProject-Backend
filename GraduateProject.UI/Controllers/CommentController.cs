1fd84a916ba5651c7f61ed10803fc92f062d8c1e
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-03-03 16:36:02 +0300

    updating CommentController and RatingController
    Adding CreateItem and UpdateItem action to both of them

--- GraduateProject.UI/Controllers/CommentController.cs
+++ GraduateProject.UI/Controllers/CommentController.cs
@@ -31,5 +31,58 @@ namespace GraduateProject.UI.Controllers
 
             return item == null ? NotFound() : Ok(item);
         }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ActionResult<Comment>> CreateItem(Comment item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                _context.Comments.Add(item);
+                await _context.SaveChangesAsync();
+
+                return Ok(item);
+
+            }
+        }
+
+        [HttpPut]
+        [Route("[action]/{id}")]
+        public async Task<IActionResult> UpdateItem(int id, Comment item)
+        {
+            if (id != item.CommentId)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Comments.Update(item);
+            _context.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id)) { return NotFound();  }
+                else { throw; }
+            }
+
+            return Ok(item);
+        }
+
+        private bool ItemExists(int id)
+        {
+            return _context.Comments.Any(e => e.CommentId == id);
+        }
+
     }
 }
