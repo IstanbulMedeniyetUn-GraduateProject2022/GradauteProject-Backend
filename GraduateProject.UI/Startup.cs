1e66341050cea79f072e4d9df75c612a4b6eeea0
BETÜL ALBAYRAK <betol.99tur@gmail.com>
2022-04-03 14:20:04 +0300

    adding mailSetting but it isn't complete yet

--- GraduateProject.UI/Startup.cs
+++ GraduateProject.UI/Startup.cs
@@ -1,3 +1,4 @@
+using GraduateProject.UI.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,7 @@ namespace GraduateProject.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
